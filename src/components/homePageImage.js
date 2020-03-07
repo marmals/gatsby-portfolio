ad58e537e259b8eafcd611ff9a981070c0ff5df5
martin <martin@marmals.localdomain>
2020-03-07 11:05:58 +0100

    fixed some homepage layout stuff

--- src/components/homePageImage.js
+++ src/components/homePageImage.js
@@ -1,11 +1,14 @@
 import React from "react"
 
 import homePageImageStyles from "../sass/components/homePageImage.module.scss"
+import FooterPanel from "./footerPanel"
+
 
 const HomePageImage = () => {
   return (
     <div className={homePageImageStyles.component}>
       <h1>I'm Martin</h1>
+      <section><FooterPanel/></section>
     </div>
     )
 }
--- src/components/homePageLayout.js
+++ src/components/homePageLayout.js
@@ -2,7 +2,6 @@ import React from "react"
 import PropTypes from "prop-types"
 
 import Header from "./header"
-import Footer from "./footer"
 
 import "../sass/layout/index.scss"
 import layoutStyles from '../sass/layout/homePageLayout.module.scss';
@@ -15,7 +14,6 @@ const HomePageLayout = ({ children }) => {
       <div className={layoutStyles.container}>
         <main>{children}</main>
       </div>
-      <Footer/>
     </>
   )
 }
